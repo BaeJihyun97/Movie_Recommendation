bf3571eb23339fdc18dcdbdcdacd68c5799e3e1e
BaeJihyn97 <baejihyun970519@gmail.com>
2022-10-18 15:29:11 +0900

    input fetch

--- frontend/src/component/Nav.js
+++ frontend/src/component/Nav.js
@@ -1,20 +1,60 @@
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../sass/Nav.scss';
+import React, {useState} from 'react';
+
+const { _, REACT_APP_BASE_BACKEND_URL } = process.env;
 
 function Nav() {
 
     const navigate = useNavigate();
 
-    const sendTitle = async () => {
-        let title = document.getElementById("title");
-        console.log(title.value);
+    const url = REACT_APP_BASE_BACKEND_URL + '/recommendation/recommend/' ;
 
-    //     await axios.get("", {
-    //         params: {title: title}
-    //     });
-    //    navigate("" + title);
-        navigate("/search");
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState({
+      movieTitle: '',
+    });
+
+    const {movieTitle} = data;
+
+    const [graphData, setGraphData] = useState([]);
+    const [imageData, setImageData] = useState([]);
+
+      const onChange = e => {
+
+        const {name, value} = e.target;
+        setData({
+        ...data,
+        [name]: value
+        });
+      };
+
+
+    const sendTitle = async () => {
+        let title = document.getElementById("movieTitle");
+        console.log(data)
+
+        fetch(url,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf8",
+            },
+            body: JSON.stringify(data),
+        }).then(response => response.json())
+        .then((response) => {
+            //const res = JSON.parse(response.data)
+            if(response.data)
+            {   setGraphData(response.data.graph);
+                setImageData(response.data.image);
+                console.log(response.data.graph)
+                setLoading(true);
+            }
+        }).catch((error)=> {
+            //console.error(error);
+            setLoading(false);
+        })
     }
 
     return (
@@ -36,7 +76,7 @@ function Nav() {
                 <div className="right-menu">
                     <div className="search-box">
                         <i className="bi bi-search" onClick={sendTitle}></i>
-                        <input id="title" name="title" placeholder="영화 제목을 입력하세요." />
+                        <input id="movieTitle" name="movieTitle" type="text" placeholder="영화 제목을 입력하세요." value={movieTitle} onChange={onChange} />
                     </div>
                     <i className="bi bi-person-fill"></i>
                 </div>
@@ -46,4 +86,9 @@ function Nav() {
   }
   
   export default Nav;
-  
+
+
+  /*
+  <input id="title" name="title" placeholder="영화 제목을 입력하세요." />
+  <i className="bi bi-search" onClick={sendTitle}></i>
+  */
